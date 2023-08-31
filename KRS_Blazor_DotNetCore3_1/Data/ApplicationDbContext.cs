2fb0948af4c648437c65101dedda9a16528203e9
Andre Kroukamp <andrekroukamp140791@gmail.com>
2023-08-31 15:22:55 +0200

    Add project files.

---
+++ KRS_Blazor_DotNetCore3_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KRS_Blazor_DotNetCore3_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
